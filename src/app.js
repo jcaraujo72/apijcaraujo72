57a17b153cbb99dfa6053d7fff37bdd6d6f081b2
Juan Carlos <jcaraujo72@hotmail.com>
2024-02-22 12:39:05 -0500

    Swagger 3

--- src/app.js
+++ src/app.js
@@ -17,6 +17,16 @@ app.use(express.json()) // Para que interprete/entienda  datos json
 
 app.use(cors())
 
+//
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", 
+  "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+//
+
+
 const swaggerSpec = swaggerJsDoc(options)
 
 app.use(indexRoutes)
