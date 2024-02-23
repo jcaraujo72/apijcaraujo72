1742eb4468811e07264505fad821627c48d7695c
Juan Carlos <jcaraujo72@hotmail.com>
2024-02-22 20:07:46 -0500

    swagger noche

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from './app.js'
 import { PORT , DB_PORT, DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD } from './config.js'
 
 app.listen(PORT)
-console.log(' DB_HOST y PORT ==> ', DB_HOST , PORT , ' - DB_DATABASE Y DB_PORT ', DB_DATABASE ,  DB_PORT)
+console.log('.... DB_HOST y PORT ==> ', DB_HOST , PORT , ' - DB_DATABASE Y DB_PORT ', DB_DATABASE ,  DB_PORT)
 console.log('USER y PASSWORD ', DB_USER , DB_PASSWORD)
